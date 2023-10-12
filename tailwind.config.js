608980e860b32d5d8695d1732941964d13253237
Martijn Storck <martijn@storck.io>
2023-10-12 15:53:33 +0200

    Safelist for VoteButton styles (to fix later)

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,4 +7,11 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  safelist: [
+    { pattern: /bg-(blue|indigo|gray)-500/, variants: ['focus'] },
+    { pattern: /border-(blue|indigo|gray)-200/ },
+    { pattern: /text-(blue|indigo|gray)-500/ },
+    { pattern: /bg-(blue|indigo|gray)-600/, variants: ['hover'] },
+    { pattern: /ring-(blue|indigo|gray)-(200|500)/, variants: ['focus'] },
+  ]
 }
